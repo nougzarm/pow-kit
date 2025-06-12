17ac460576414720c9147ee8a61fc95b5b3fd98c
nougzarm <nougzarm@icloud.com>
2025-06-12 17:06:47 +0200

    feat(trait): define ProofOfWork trait
    - feat: implement ProofOfWork trait definition for proof-of-work functionality

--- src/traits.rs
+++ src/traits.rs
@@ -1,1 +1,14 @@
+//! [`ProofOfWork`] Trait.
+//!
+//! This module defines the [`ProofOfWork`] trait, a generic interface for proof of work protocols.
 
+pub trait ProofOfWork {
+    type Challenge;
+    type Solution;
+
+    fn generate_challenge(&self) -> Self::Challenge;
+
+    fn solve(&self, challenge: &Self::Challenge) -> Self::Solution;
+
+    fn verify(&self, challenge: &Self::Challenge, solution: &Self::Solution) -> bool;
+}
